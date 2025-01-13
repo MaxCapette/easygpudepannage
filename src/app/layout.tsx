7473878e1d8da89c0b958a6850c5e0fcd429804e
Dokuyaku <sk8baz@gmail.com>
2025-01-13 20:57:52 +0100

    changed metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,9 @@ import { Footer } from "@/components/footer"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "Easy-gpu | Dépannage Informatique Derval",
-  description: "Expert en dépannage et maintenance informatique",
+  title: 'Easy-gpu | Expert en dépannage informatique à Derval',
+  description: 'Solutions rapides et professionnelles pour tous vos problèmes informatiques. Intervention à domicile à Derval et ses alentours ou à distance.',
+  keywords: ['dépannage informatique', 'maintenance informatique', 'Derval', 'PC Gamer', 'réparation ordinateur'],
 }
 
 export default function RootLayout({

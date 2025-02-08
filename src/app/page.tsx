21fdcbb11fc619f88870c0248061df59af8bbc21
Dokuyaku <sk8baz@gmail.com>
2025-02-08 13:40:51 +0100

    add website creation service

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 
-import { Button } from "@/components/ui/button"
+
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Check, Cpu, HardDrive, Monitor, Shield, Star, Wrench } from 'lucide-react'
 
